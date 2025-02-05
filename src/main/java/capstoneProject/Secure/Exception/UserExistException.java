ee5348cd0f78a09f515df980d9f67076933a3226
RAKSHITH R <108052912+RakshithR07@users.noreply.github.com>
2025-02-05 18:54:07 +0530

    removed the duplicate file

--- src/main/java/capstoneProject/Secure/Exception/UserExistException.java
+++
@@ -1,7 +0,0 @@
-package capstoneProject.Secure.Exception;
-
-public class UserExistException extends RuntimeException{
-    public UserExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/capstoneProject/Secure/repo/AdminDumpRepo.java
+++
@@ -1,8 +0,0 @@
-package capstoneProject.Secure.repo;
-
-import capstoneProject.Secure.model.AdminDump;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminDumpRepo extends JpaRepository<AdminDump,Long> {
-
-}
